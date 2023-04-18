2fdcf2e6eb134cc0621500469a38163ad82d3035
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-04-18 23:58:42 +0100

    Fix url field in kubectl-mayastor cask

--- Casks/kubectl-mayastor.rb
+++ Casks/kubectl-mayastor.rb
@@ -2,8 +2,8 @@ cask "kubectl-mayastor" do
   version "2.0.1"
   sha256 "085a64ae9b91d7ea22037cd0a095a0c0e936e5550b921e5a1cb2b089f7af45d4"
 
-  url "https://github.com/openebs/mayastor-control-plane/releases/download/v#{version}/kubectl-mayastor-x86_64-apple-darwin.zip,
-      verified: github.com/openebs/mayastor-control-plane/"
+  url "https://github.com/openebs/mayastor-control-plane/releases/download/v#{version}/kubectl-mayastor-x86_64-apple-darwin.zip",
+    verified: "github.com/openebs/mayastor-control-plane/"
   name "Mayastor Kubectl Plugin"
   desc "Kubectl plugin to view/manage Mayastor resources"
   homepage "https://mayastor.gitbook.io/"
